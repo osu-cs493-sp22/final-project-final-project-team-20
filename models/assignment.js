236343358358120e1c78b21356f18f1478cbea90
Hershbja <ander.the.enderwolf@gmail.com>
2022-06-04 12:39:32 -0700

    main data changes, adjustments
    .data/assignments created and completed
    .data/courses created and completed
    .data/submissions created and completed
    .data/users created and completed
    
    .models/assignment changed due datatype to String
    .models/submission changed timestamp datatype to String

--- models/assignment.js
+++ models/assignment.js
@@ -7,7 +7,7 @@ const Assignment = sequelize.define('assignment', {
   courseId: { type: DataTypes.INTEGER, allowNull: false },
   title: { type: DataTypes.TEXT, allowNull: false },
   points: { type: DataTypes.INTEGER, allowNull: false },
-  due: { type: DataTypes.DATE, allowNull: false }
+  due: { type: DataTypes.STRING, allowNull: true }
 })
 
 /*
@@ -26,5 +26,5 @@ exports.AssignmentClientFields = [
   'courseId',
   'title',
   'points',
-  'due'
+  'due' //Time format is ISO 8601 (2022-06-14T17:00:00-07:00 is Jun 15, 2022, 12:00:00 AM [UTC])
 ]
--- models/submission.js
+++ models/submission.js
@@ -8,7 +8,7 @@ const Submission = sequelize.define('submission', {
   studentId: { type: DataTypes.INTEGER, allowNull: false },
   grade: { type: DataTypes.FLOAT, allowNull: false },
   file: { type: DataTypes.STRING, allowNull: false },
-  timestamp: { type: DataTypes.DATE, allowNull: false, defaultValue: Date.now() }
+  timestamp: { type: DataTypes.STRING, allowNull: false, defaultValue: sequelize.literal('NOW()').toString() }
 })
 
 /*
